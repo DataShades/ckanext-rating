14cee23c4f59ed3e5c615241042f591c513d5dbe
Mark Gregson <mark.gregson@linkdigital.com.au>
2016-12-06 17:48:41 +1100

    SUNCORPXCEBD-108 always show full star on hover

--- ckanext/rating/public/js/rating.js
+++ ckanext/rating/public/js/rating.js
@@ -1,10 +1,10 @@
 $('.rating-star').hover(
   function(){ 
-    $(this).addClass('rating-star-hover') 
-    $(this).prevAll().addClass('rating-star-hover') 
+    $(this).addClass('rating-star-hover icon icon-star')
+    $(this).prevAll().addClass('rating-star-hover icon icon-star')
   },
   function(){ 
-    $(this).removeClass('rating-star-hover') 
-    $(this).prevAll().removeClass('rating-star-hover') 
+    $(this).removeClass('rating-star-hover icon icon-star')
+    $(this).prevAll().removeClass('rating-star-hover icon icon-star')
   }
 )
